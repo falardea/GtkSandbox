3b3ee831e7a85afb5faa0a097fbb7efcd0304d6c
Richard Falardeau <rfalarde@nrao.edu>
2024-03-17 11:26:56 -0600

    May have added the StackSwitcher on the last commit, or it's this one... it looks meh, so I kept my buttons and setting the page will do the same... just don't hide them, or they aren't on the stack anymore.

--- src/main.c
+++ src/main.c
@@ -20,12 +20,23 @@ void exit_test(void) {
    }
 }
 
+GSourceFunc some_task_on_timeout(gpointer user_data)
+{  /* An example of a timeout function, just because I was playing with it...
+    */
+   logging_llprintf(LOGLEVEL_INFO, "%s: CHECKPOINT \n", __func__);
+   return G_SOURCE_REMOVE;
+}
+
+
 int main(int argc, char **argv) {
    printf("ver.%d.%d.%d\n", project_VERSION_MAJOR, project_VERSION_MINOR, project_VERSION_PATCH);
+
+   g_timeout_add(5000, (GSourceFunc)some_task_on_timeout, NULL);
 
    app_init(argc, argv);
 
    if (getAppModelInitState() != INIT_ERROR) {
+
       logging_llprintf(LOGLEVEL_DEBUG, "application initialized, starting UI\n");
       gtk_main();
       logging_llprintf(LOGLEVEL_DEBUG, "UI closed, shutting down\n");
--- src/views/view_wizard/view_wizard.c
+++ src/views/view_wizard/view_wizard.c
@@ -4,35 +4,50 @@
 #include "view_wizard.h"
 #include "../../utils/logging.h"
 
-void on_btn_HorizontalPair_next_clicked(__attribute__((unused)) GtkButton *button, __attribute__((unused)) gpointer *user_data)
+void on_btn_HorizontalPair_next_clicked(__attribute__((unused)) GtkButton *button, gpointer *user_data)
 {
+   AppWidgets_T *wdgts = (AppWidgets_T *) user_data;
    logging_llprintf(LOGLEVEL_DEBUG, "%s\n", __func__);
 }
-void on_btn_HorizontalPair_back_clicked(__attribute__((unused)) GtkButton *button, __attribute__((unused)) gpointer *user_data)
+void on_btn_HorizontalPair_back_clicked(__attribute__((unused)) GtkButton *button, gpointer *user_data)
 {
+   AppWidgets_T *wdgts = (AppWidgets_T *) user_data;
    logging_llprintf(LOGLEVEL_DEBUG, "%s\n", __func__);
+   gtk_stack_set_visible_child(GTK_STACK(wdgts->w_stck_SetupWizard), wdgts->w_stckpg_HorizontalPair1);
 }
-void on_btn_GridPage_next_clicked(__attribute__((unused)) GtkButton *button, __attribute__((unused)) gpointer *user_data)
+void on_btn_GridPage_next_clicked(__attribute__((unused)) GtkButton *button, gpointer *user_data)
 {
+   AppWidgets_T *wdgts = (AppWidgets_T *) user_data;
    logging_llprintf(LOGLEVEL_DEBUG, "%s\n", __func__);
+   gtk_stack_set_visible_child(GTK_STACK(wdgts->w_stck_SetupWizard), wdgts->w_stckpg_VerticalPair);
 }
-void on_btn_GridPage_back_clicked(__attribute__((unused)) GtkButton *button, __attribute__((unused)) gpointer *user_data)
+void on_btn_GridPage_back_clicked(__attribute__((unused)) GtkButton *button, gpointer *user_data)
 {
+   AppWidgets_T *wdgts = (AppWidgets_T *) user_data;
    logging_llprintf(LOGLEVEL_DEBUG, "%s\n", __func__);
+   gtk_stack_set_visible_child(GTK_STACK(wdgts->w_stck_SetupWizard), wdgts->w_stckpg_HorizontalPair);
 }
-void on_btn_VerticalPair_back_clicked(__attribute__((unused)) GtkButton *button, __attribute__((unused)) gpointer *user_data)
+void on_btn_VerticalPair_back_clicked(__attribute__((unused)) GtkButton *button, gpointer *user_data)
 {
+   AppWidgets_T *wdgts = (AppWidgets_T *) user_data;
    logging_llprintf(LOGLEVEL_DEBUG, "%s\n", __func__);
+   gtk_stack_set_visible_child(GTK_STACK(wdgts->w_stck_SetupWizard), wdgts->w_stckpg_GridPage);
 }
-void on_btn_VerticalPair_next_clicked(__attribute__((unused)) GtkButton *button, __attribute__((unused)) gpointer *user_data)
+void on_btn_VerticalPair_next_clicked(__attribute__((unused)) GtkButton *button, gpointer *user_data)
 {
+   AppWidgets_T *wdgts = (AppWidgets_T *) user_data;
    logging_llprintf(LOGLEVEL_DEBUG, "%s\n", __func__);
+   gtk_stack_set_visible_child(GTK_STACK(wdgts->w_stck_SetupWizard), wdgts->w_stckpg_HorizontalPair1);
 }
-void on_btn_HorizontalPair_next1_clicked(__attribute__((unused)) GtkButton *button, __attribute__((unused)) gpointer *user_data)
+void on_btn_HorizontalPair_next1_clicked(__attribute__((unused)) GtkButton *button, gpointer *user_data)
 {
+   AppWidgets_T *wdgts = (AppWidgets_T *) user_data;
    logging_llprintf(LOGLEVEL_DEBUG, "%s\n", __func__);
+   gtk_stack_set_visible_child(GTK_STACK(wdgts->w_stck_SetupWizard), wdgts->w_stckpg_HorizontalPair);
 }
-void on_btn_HorizontalPair_back1_clicked(__attribute__((unused)) GtkButton *button, __attribute__((unused)) gpointer *user_data)
+void on_btn_HorizontalPair_back1_clicked(__attribute__((unused)) GtkButton *button, gpointer *user_data)
 {
+   AppWidgets_T *wdgts = (AppWidgets_T *) user_data;
    logging_llprintf(LOGLEVEL_DEBUG, "%s\n", __func__);
+   gtk_stack_set_visible_child(GTK_STACK(wdgts->w_stck_SetupWizard), wdgts->w_stckpg_VerticalPair);
 }
